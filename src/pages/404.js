9648cd5d3b5c3e1808821a16210ff223da249545
gorillahobo <galihwicaksono90@gmail.com>
2021-02-04 15:11:57 +0700

    Customize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,28 @@
 import React from "react"
+import styled from "@emotion/styled"
+import { Helmet } from "react-helmet"
 
-export default () => <h1>404 not found</h1>
+import Layout from "../components/Layout"
+
+const Styled404 = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  & h1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+`
+
+export default () => (
+  <Layout>
+    <Helmet>
+      <title>Goblorg | 404</title>
+    </Helmet>
+    <Styled404>
+      <h1>404 Page not found</h1>
+    </Styled404>
+  </Layout>
+)
